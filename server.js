3851f9447ef8ccd19dc7b0cd3d4d207db2093a8b
Jaime Valdivia <jaivaldivia@gmail.com>
2016-10-29 20:15:06 -0500

    Adding dev db information

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
+var mongoose   = require('mongoose');
 
+var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); //Get data from POST
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://node:node@ds031257.mlab.com:31257/jaime-dev'); // connect to our dev database
 
 /*
  * Routes
